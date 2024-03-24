0bf70831001bb161ff31e8b8e14523c89e1dd7ad
ilya-bkv <i.yablokov@ccmp.me>
2024-03-24 14:51:38 +0300

    Update poster

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import pomegranate from './assets/pomegranate.png'
+// import pomegranate from './assets/pomegranate.png'
+import afisha from './assets/afisha.jpeg'
 import styles from './App.module.scss'
 
 function App() {
 
   return (
     <>
-      <div className={styles.pomegranate} style={{backgroundImage: `url(${pomegranate})`}}/>
+      <div className={styles.pomegranate} style={{backgroundImage: `url(${afisha})`}}/>
       <div className={styles.heroWrapper}>
-        <div className={styles.text}>
-          <h1 className={styles.glitch} data-text={'The Sound of Pomegranates'}>The Sound of Pomegranates</h1>
-          <div className={styles.hr}/>
-          <h2 className={styles.glitch} data-text={'Նռան ձայնը'}>Նռան ձայնը</h2>
-          <div className={styles.subtitle}>
-            <div>[28/03/2024 7PM] Common Ground</div>
-            <div>Evening of experimental music</div>
-          </div>
-        </div>
+        {/*<div className={styles.text}>*/}
+          {/*<h1 className={styles.glitch} data-text={'The Sound of Pomegranates'}>The Sound of Pomegranates</h1>*/}
+          {/*<div className={styles.hr}/>*/}
+          {/*<h2 className={styles.glitch} data-text={'Նռան ձայնը'}>Նռան ձայնը</h2>*/}
+          {/*<div className={styles.subtitle}>*/}
+          {/*  <div>[28/03/2024 7PM] Common Ground</div>*/}
+          {/*  <div>Evening of experimental music</div>*/}
+          {/*</div>*/}
+        {/*</div>*/}
       </div>
     </>
   )
